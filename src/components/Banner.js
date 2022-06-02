fd4ed50f29b1f193e1b70d949896f99206e11d3a
Noman01 <neamulhaque2002@gmail.com>
2022-06-03 00:27:29 +0600

    banner bg added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,17 +3,21 @@ import './style.css'
 
 const Banner = () => {
     return (
-        <section class="hero min-h-screen bg-base-200 py-8 md:py-12">
-            <div class=" max-w-screen-xl hero-content flex-col-reverse lg:flex-row-reverse gap-16">
+        <section
+            style={{
+                backgroundImage: 'url(banner.jpg)'
+            }}
+            class="hero min-h-screen bg-base-200 py-8 md:py-12 text-white" >
+            <div class=" max-w-screen-xl hero-content flex-col-reverse lg:flex-row gap-16">
                 <div class="text-center lg:text-left">
                     <p><span className=''>Hello,</span></p>
                     <h1 class="text-5xl font-bold"><span className='hover:italic'>I am Neamul</span> <br /> <span className='text-primary'>MERN Stack Web Developer</span> </h1>
                     <p class="py-6 pr-44">
                         I am a Pationate web <span className="font-bold">developer</span>. Web Development is my pation programming is my everyday must working routin.
                     </p>
-                    <button className="btn bg-secondary custom-box-shadow mt-6 hover:bg-primary custom-animation hover:border-none">Download CV</button>
+                    <button className="btn bg-orange-500 border-none custom-box-shadow mt-6 hover:bg-orange-600 custom-animation hover:border-none">Download CV</button>
                 </div>
-                <div class="card m-0 p-0 flex-shrink-0 w-full hover:cursor-zoom-in max-w-sm custom-box-shadow  zom" >
+                <div class="card m-0 p-0 flex-shrink-0 w-full hover:cursor-zoom-in max-w-sm bg-[#cbd3ea]  custom-box-shadow  zom" >
                     <img className='w-full' src="Noman.png" alt="Profile" />
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       {
         mytheme: {
           primary: "#3B82FC",
-          secondary: "#252525a8",
+          secondary: "rgb(249, 115, 22)",
           accent: "#37cdbe",
           neutral: "#3d4451",
           "base-100": "#ffffff",

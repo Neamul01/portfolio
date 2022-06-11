bfac39f1d31cb798e9812145da6da6945953e5f2
Noman01 <neamulhaque2002@gmail.com>
2022-06-11 14:08:37 +0600

    back button created

--- src/components/Home/ProjectDetails.js
+++ src/components/Home/ProjectDetails.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import useFetchdata from '../hooks/useFetchdata';
 
 const ProjectDetails = () => {
+    const navigate = useNavigate();
     const { id } = useParams();
     const [projects] = useFetchdata(id);
     // console.log(projects)
@@ -10,6 +12,9 @@ const ProjectDetails = () => {
     const singleProject = projects.find(project => project.id === Number(id));
     return (
         <div className="hero min-h-screen bg-base-200">
+            <button
+                onClick={() => navigate(-1)}
+                className="btn btn-success fixed top-0 left-0 z-10">Back</button>
             <div className="hero-content flex-col lg:flex-row">
                 <img className='max-w-lg' src={singleProject?.image} alt='project' />
                 <div>
